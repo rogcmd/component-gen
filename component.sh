323345a74bb87c53f57a37e56d32d94f42b28feb
rog <pessoal.rog@gmail.com>
2026-04-08 05:56:52 -0300

    feat: check if the name of the component is null

--- component.sh
+++ component.sh
@@ -1,61 +1,64 @@
 #!/usr/bin/env bash
 
-echo ""
 gum=$(which gum)
 if [ ! -n "$gum" ]; then
-    echo "this program require Gum to load!"
-    echo "dowload Gum at: https://github.com/charmbracelet/gum?tab=readme-ov-file#installation"
-else
+    echo ""
+    echo -e "\e[31mThis program require Gum to load!\e[0m"
+    echo -e "Check how to install Gum at: \e[34mhttps://github.com/charmbracelet/gum?tab=readme-ov-file#installation\e[0m"
+  else
   gum style \
   --background "#cba6f7" \
   --foreground "#1e1e2e" \
   --padding "0 1" \
+  --margin "1 0" \
   --bold \
   "components"\
-  
-  echo ""
   
   CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
   escolha=$(gum choose "astro component" "reset.css" --header "What you wanna create ?")
   case $escolha in
-      "astro component")
-        ComponentName=$(gum input --header "what component u wanna make?" --placeholder "button")
-        Component="${ComponentName^}.astro"
-          
-        if gum confirm "generate $Component ?"; then
-          if [ ! -d "./src/components" ]; then
-            mkdir -p ./src/components
-          fi
-          cp $CURRENT_PATH/files/component.astro "./src/components/$Component"
-        fi
-        if [ -f "./src/components/$Component" ]; then
-        gum style \
-        --foreground "#94e2d5" \
-        "file created at src/components"
-        tree . -P "$Component" -L 3--prune
-        else 
-          clear
-          gum style \
-          --foreground "#f38ba8" \
-          "failed to create file"
-        fi
-        ;;
+    "astro component")
+      ComponentName=$(gum input --header "what component u wanna make?" --placeholder "button")
+
+      while [ -z "$ComponentName" ]; do
+      echo -e "\e[31mTitle cant be blank!\e[0m"
+      TITLE=$(gum input --header "Title of the commit:" --placeholder "changes you made")
+      done
+      
+      Component="${ComponentName^}.astro"
         
-      "reset.css")
-        if [ ! -d "./src/styles" ]; then
-          mkdir -p ./src/styles
+      if gum confirm "generate $Component ?"; then
+        if [ ! -d "./src/components" ]; then
+        mkdir -p ./src/components
         fi
-        cp $CURRENT_PATH/files/reset.css ./src/styles/
-        if [ -f "./src/styles/reset.css" ]; then
+        cp $CURRENT_PATH/files/component.astro "./src/components/$Component"
+      fi
+      if [ -f "./src/components/$Component" ]; then
+      gum style \
+      --foreground "#94e2d5" \
+      "File created at src/components"
+      else 
+        clear
         gum style \
-        --foreground "#94e2d5" \
-        "file created at src/styles"
-        tree . -P 'reset.css' -L 3 --prune
-        else 
-          gum style \
-          --foreground "#f38ba8" \
-          "failed to create file"
-        fi
-        ;;
+        --foreground "#f38ba8" \
+        "Operation canceled"
+      fi
+      ;;
+      
+    "reset.css")
+      if [ ! -d "./src/styles" ]; then
+        mkdir -p ./src/styles
+      fi
+      cp $CURRENT_PATH/files/reset.css ./src/styles/
+      if [ -f "./src/styles/reset.css" ]; then
+      gum style \
+      --foreground "#94e2d5" \
+      "File created at src/styles"
+      else 
+        gum style \
+        --foreground "#f38ba8" \
+        "failed to create file"
+      fi
+      ;;
   esac
 fi
